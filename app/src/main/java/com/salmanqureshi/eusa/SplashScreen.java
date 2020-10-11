9a706ffddc182821588770d0ad1e7d10b5478f49
akashali506 <i170019@nu.edu.pk>
2020-10-11 22:47:02 +0500

    Splash screen

--- app/src/main/java/com/salmanqureshi/eusa/SplashScreen.java
+++ app/src/main/java/com/salmanqureshi/eusa/SplashScreen.java
@@ -3,14 +3,34 @@ package com.salmanqureshi.eusa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
     public class SplashScreen extends AppCompatActivity {
+        Animation topAnim, bottomAnim;
+        TextView logoName;
+        ImageView logoIcon;
+        private static int SPLASH_SCREEN=2000;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splash_screen);
+            topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+            bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+            logoIcon = findViewById(R.id.logoicon);
+            logoName = findViewById(R.id.logoname);
+
+            logoName.setAnimation(bottomAnim);
+            logoIcon.setAnimation(topAnim);
+
+
             Handler handler=new Handler();
             handler.postDelayed(new Runnable() {
                 @Override
@@ -19,5 +39,6 @@ import androidx.appcompat.app.AppCompatActivity;
                     startActivity(intent);
                     finish();
                 }
-            },1000);        }
+            },SPLASH_SCREEN);
+        }
     }
